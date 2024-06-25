13bc7ec005cb326ed4308ed14ec5549ba71a78f8
Ali3905 <muhammadali30905@gmail.com>
2024-06-25 15:17:19 +0500

    add api to get package booking by ID

--- routes/packageBooking.js
+++ routes/packageBooking.js
@@ -1,9 +1,10 @@
 const express = require("express")
-const { handleCreatePackageBooking, handleGetAllPackageBookings, handleDeletePackageBooking } = require("../controllers/packageBooking")
+const { handleCreatePackageBooking, handleGetAllPackageBookings, handleDeletePackageBooking, handleGetPackageBookingByID } = require("../controllers/packageBooking")
 const router = express.Router()
 
 router.post("/", handleCreatePackageBooking)
 router.get("/", handleGetAllPackageBookings)
+router.get("/:bookingId", handleGetPackageBookingByID)
 router.delete("/", handleDeletePackageBooking)
 
 
