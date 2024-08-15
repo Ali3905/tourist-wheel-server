3fa68520bec095e315541da2b1041cd48b4408db
Ali3905 <muhammadali30905@gmail.com>
2024-08-15 06:25:28 +0500

    add middleware for images

--- routes/busRoute.js
+++ routes/busRoute.js
@@ -10,7 +10,7 @@ router.get("/", handleGetAllBusRoutes)
 // To Test
 router.patch("/finalize", handleFinalizeBusRoute)
 router.delete("/", handleDeleteBusRoute)
-router.patch("/", handleUpdateBusRoute)
+router.patch("/", upload.fields([{ name: "busPhotos", maxCount: 5 }, { name: "seatingArrangement", maxCount: 1 }, { name: "QR", maxCount: 1 }]), handleUpdateBusRoute)
 router.patch("/start", upload.fields([{ name: "beforeJourneyPhotos", maxCount: 5 }]), handleStartBusRoute)
 router.patch("/complete", upload.fields([, { name: "afterJourneyPhotos", maxCount: 5 }]), handleCompleteBusRoute)
 router.get("/driver/:driverId", handleGetDriverBusRoutes)
